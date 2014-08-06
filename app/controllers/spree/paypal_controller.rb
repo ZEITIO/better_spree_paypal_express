198ec1c47bb151df8c05a7765b23f1375fed992b
Fernando Sainz <fsainz@gmail.com>
2014-08-06 19:25:21 +0200

    typo

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -168,7 +168,7 @@ module Spree
     end
 
     def completion_route(order)
-      order_path(order, :token => order.guess_token)
+      order_path(order, :token => order.guest_token)
     end
 
     def address_required?
